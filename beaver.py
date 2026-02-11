47eec615564a32d315e9ea93ee942c0b5e036d4f
timc-odoo <timc@odoo.com>
2026-02-11 08:43:17 -0500

    Moved check flag

--- beaver.py
+++ beaver.py
@@ -8,6 +8,7 @@ import datetime
 filename = sys.argv[1]
 dbname = sys.argv[2]
 print(filename)
+print(dbname)
 date = datetime.date.today()
 date = str(date)
 #dbname = input("Enter database name: ")
@@ -20,10 +21,10 @@ with open(filename) as old:
     for line in old:
         cur_line = line
         if dbname in cur_line or flag is True:
-            newfile.write(cur_line)
             if "2026" in cur_line:
                 #print(cur_line)
                 flag = False
             if "ERROR" in cur_line:
                 flag = True
                 #print(flag)
+            newfile.write(cur_line)
